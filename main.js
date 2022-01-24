4abea4f059e6bbc6d40235f19180b38186a082f3
marco maione <maionemarco14@gmail.com>
2022-01-24 16:07:13 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
 let price = km * 0.21;
-let discount = price / 100 * 20 ;
 let senior = price / 100 * 40 ;
-let finalPrice = price - discount
 let name = document.getElementById("nome cognome");
 let km = document.getElementById("km da percorrere");
-let age = document.getElementById("eta")
+let age = document.getElementById(" Fascia d'eta");
+let button = document.getElementById("genera")
+
 
 if ( age = minorenne) {
+    let discount = price / 100 * 20 ;
+    var finalPrice = price - discount;
+    document.getElementById("costo").innerHTML = finalPrice + "euro";
     
 }
 
