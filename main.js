157657a1aa5711f978979da921ee385a16416e0e
marco maione <maionemarco14@gmail.com>
2022-01-24 19:27:51 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 let button = document.getElementById("genera")
 
-button.addEventListener('click ',
+button.addEventListener('click',
     function() {
+        let km = document.getElementById("km").value;
         let price = km * 0.21;
-        let name = document.getElementById("name");
-        let km = document.getElementById("km");
-        let age = document.getElementById("age");
+        let name = document.getElementById("name").value;
+        let age = document.getElementById("age").value;
+        document.querySelector('.second').style.display='flex';
 
-        if ( age = minorenne) {
+        let posto = Math.floor(Math.random() * 10)
+        let cp = Math.floor(Math.random() * (100000 - 90000) + 90000);
+        console.log(km)
+        console.log(name)
+        console.log(age)
+
+        if ( age == "minorenne") {
             let discount = price / 100 * 20 ;
-            var finalPrice = price - discount;
-            document.getElementById("costo").value = finalPrice + "€";
-        }   
+            let finalPrice = price - discount;
+            document.getElementById("costo").innerHTML = finalPrice + "€";
+            price.toFixed(2);
+            document.getElementById('offerta').innerHTML = "biglietto scontato"
+            
         
-    }
+        } else if ( age == "pensionato") {
+            let senior = price /100 * 40 ;
+            let finalPrice = price - senior;
+            document.getElementById("costo").innerHTML = finalPrice + "€";
+            price.toFixed(2);
+            document.getElementById('offerta').innerHTML = "biglietto scontato"
+
+        } else {
+            document.getElementById("costo").innerHTML = price + "€";
+            price.toFixed(2);
+            document.getElementById('offerta').innerHTML = "biglietto standard"
+        }  
+        
+        document.getElementById('name_output').innerHTML = name;
+        document.getElementById('n-carrozza').innerHTML = posto;
+        document.getElementById('cp').innerHTML = cp;
+        
+    })
