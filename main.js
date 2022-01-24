7beab9d7c0ae2eaeb6c3593a794f0cc729d6d517
marco maione <maionemarco14@gmail.com>
2022-01-24 16:43:04 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-
-let price = km * 0.21;
-let senior = price / 100 * 40 ;
-let name = document.getElementById("nome cognome");
-let km = document.getElementById("km da percorrere");
-let age = document.getElementById(" Fascia d'eta");
 let button = document.getElementById("genera")
 
+button.addEventListener('click ',
+    function() {
+        let price = km * 0.21;
+        let name = document.getElementById("name");
+        let km = document.getElementById("km");
+        let age = document.getElementById("age");
 
-if ( age = minorenne) {
-    let discount = price / 100 * 20 ;
-    var finalPrice = price - discount;
-    document.getElementById("costo").innerHTML = finalPrice + "euro";
-    
-}
-
-
+        if ( age = minorenne) {
+            let discount = price / 100 * 20 ;
+            var finalPrice = price - discount;
+            document.getElementById("costo").value = finalPrice + "€";
+        }   
+        
+    }
